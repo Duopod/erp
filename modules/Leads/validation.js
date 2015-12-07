6812fcedccc6ae9f4ff2402c458ec58f1c4a933c
andy <s.anmoldeep1993@gmail.com>
2015-12-07 21:28:58 +0530

    Disabled Past Dates Selection in Follow Up dates in Leads Module.

--- modules/Leads/validation.js
+++ modules/Leads/validation.js
@@ -8,15 +8,18 @@ function customSaveFunction() {
 
     //Get current date
     var currentdate = new Date();
-    var datetime = "Now: " + currentdate.getDate() + "/"
-        + (currentdate.getMonth() + 1) + "/"
-        + currentdate.getFullYear() + " @ "
-        + currentdate.getHours() + ":"
-        + currentdate.getMinutes() + ":"
-        + currentdate.getSeconds();
+    var now = ("0" + (currentdate.getMonth() + 1)).slice(-2) + "/"
+        + ("0" + currentdate.getDate()).slice(-2) + "/"
+        + currentdate.getFullYear();
+// Reformat the date
+    var date_split = $('#follow_up_date_c_date').val().split('-');
+    var set_m = date_split[1];
+    var set_d = date_split[0];
+    var set_y = date_split[2];
+    var set_date = set_m + "/" + set_d + "/" + set_y;
 
 
-    // For making Assignment Remark Mandatory if Lead is Assigned to someone else
+    //  For making Assignment Remark Mandatory if Lead is Assigned to someone else
     if ($('#re_assign_comment_c').is(":visible") && $('#re_assign_comment_c').val() == "") {
         isOK = false;
         alert("Please enter a re assign Remark / Reason");   // Display Error
@@ -31,7 +34,15 @@ function customSaveFunction() {
 
         if ($('#follow_up_date_c').val() != "" && $('#lead_score_c').val() == 'dead_lead') {
             isOK = false;
+
             alert("You cannot set a Follow Up Date on a Dead Lead");   // Display Error
+            return false;
+        } else {
+        }
+// Prevent Past Dates
+        if (set_date < now) {
+            isOK = false;
+            alert("You cannot set a date in the Past!");   // Display Error
             return false;
         } else {
         }
